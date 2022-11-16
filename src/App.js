32dfe6a1e7152d05172719a62216ade1ab0fb16a
i23002 <indradeep.halder23@gmail.com>
2022-11-16 11:21:34 +0530

    basic funcitionality ready

--- src/App.js
+++ src/App.js
@@ -1,42 +1,89 @@
+/* eslint-disable no-unused-vars */
 import React from 'react';
 import './App.css';
+import axios from 'axios';
 
 // eslint-disable-next-line no-unused-vars
-var api = ''
 class App extends React.Component{
-  // eslint-disable-next-line no-useless-constructor
-  constructor(props){
-    super(props)
-    this.state = {
-      latitude : 0,
-      longitude : 0,
-      items : []
-    }
-    this.componentDidMount = this.componentDidMount.bind(this);
-  }
-// axios install and make api work 
-  componentDidMount(){
-    navigator.geolocation.getCurrentPosition((position) =>{
-      this.setState({
-        latitude : position.coords.latitude,
-        longitude: position.coords.longitude
-      })
-       api = `https://fcc-weather-api.glitch.me/api/current?lat=${position.coords.latitude}&lon=${position.coords.longitude}`;
-  })}
-    render()
-    {
-      return(
-        <div>
-          <h1>Hello  world</h1>
-          <button>Click</button>
-          <h2>Longitude: {this.state.latitude}</h2>
-          <h3>Latitude: {this.state.longitude}</h3>
-        
-          
-        </div>
-      )
-    }
-}
 
+    constructor(props) {
+
+        super(props);
+    
+        this.state = {
+            lat: 0,
+            lon: 0,
+            temp : 0,
+            humidity: 0,
+            city: '',
+            country: '',
+            sunrise: '',
+            sunset : '',
+            weather: '',
+            windspeed: ''
+
+        };
+    
+      }
+      //the most interesting section i learned really appreciate it
+      componentDidMount() {
+        if (navigator.geolocation){
+          this.getPosition()//if the promise got fullfilled then call the getweather
+          .then((position) =>{
+              this.getWeather(position.coords.latitude , position.coords.longitude)
+          })
+          .catch((err) => {// other wise call the catch 
+            this.getWeather(28.67 , 58.77);
+            alert('Enable the location ')
+          })
+        }else{
+          alert("Geolocation Not Available")
+        }  
+    }
+    getPosition = (options) =>{
+       return new Promise(function (resolve , reject){
+        navigator.geolocation.getCurrentPosition(resolve, reject, options)
+       })
+    }
+    getWeather = async(lat , lon) =>{
+         const api_call = axios.get(`https://weather-proxy.freecodecamp.rocks/api/current?lat=${lat}&lon=${lon}`)
+         .then(response => {
+          const data = response.data;
+          this.setState({
+            lat : lat,
+            lon: lon,
+            temp: Math.round(data.main.temp),
+            humidity: data.main.humidity,
+            city: data.name,
+            country: data.sys.country,
+            sunrise: data.sys.sunrise,
+            sunset: data.sys.sunset,
+            weather: data.weather[0].main,
+            windspeed: data.wind.speed
+            
+          })
+         })
+    }
+    render(){
+              return (
+        
+              <div>
+                <h1>{this.state.lat}</h1>
+                <h1>{this.state.lon}</h1>
+                <h1>Temparature: {this.state.temp}</h1>
+                <h1>Humidity: {this.state.humidity}</h1>
+                <h1>City: {this.state.city}</h1>
+                <h1>Country: {this.state.country}</h1>
+                <h1>weather: {this.state.weather}</h1>
+                <h1>WIND SPEED: {this.state.windspeed}</h1>
+                <h4>Using geolocation JavaScript API in React</h4>
+        
+              </div>
+        
+            );
+        
+          }
+    
+}
 
 export default App;
